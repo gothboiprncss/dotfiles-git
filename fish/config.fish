0ee51dc6483ba73344878bac4925c326edd8f06b
gothboiprncss <163460517+gothboiprncss@users.noreply.github.com>
2024-06-01 04:00:00 +0000

    Update 0.3 fish and waybar

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,10 @@
+function fish_prompt
+    string join '' -- (set_color green) (prompt_pwd) (set_color normal) ' (^◕ᴥ◕^)→ '
+end
+
+#Start up scripts
 function fish_greeting
      pokemon-colorscripts -r 1 --no-title
 end
+
+
